a7680589d5956cd4bef5beedb4b92cfe93a4d41e
Chi_Tang <me@chitang.dev>
2023-06-17 03:58:18 +0000

    WHY

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,23 +7,24 @@ ssh_private_key=$3
 
 pacman -Sy base-devel git --noconfirm
 
-useradd -m -s /bin/bash builder
+useradd -s /bin/bash builder
 echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
-chmod 777 $(pwd)
+chmod 777 -R $(pwd)
+
+if [ "$push" = "true" ]; then
+  echo ${ssh_private_key} > ~/.ssh/id_ed25519
+  chmod 600 ~/.ssh/id_ed25519
+  ssh-keyscan -t rsa aur.archlinux.org >> ~/.ssh/known_hosts
+  chmod 644 ~/.ssh/known_hosts
+  git config --global user.email "aur@github-actions"
+  git config --global user.name "GitHub Actions"
+fi
 
 su builder -c "git clone https://aur.archlinux.org/${package}.git"
 su builder -c "cd ${package} && makepkg -s --noconfirm && rm .SRCINFO && makepkg --printsrcinfo > .SRCINFO"
 
 if [ "$push" = "true" ]; then
-  echo ${ssh_private_key} > ~/.ssh/private
-  chmod 600 ~/.ssh/private
-  eval "$(ssh-agent -s)"
-  ssh-add ~/.ssh/private
-  ssh-keyscan -t rsa aur.archlinux.org >> ~/.ssh/known_hosts
-  chmod 644 ~/.ssh/known_hosts
-  git config --global user.email "aur@github-actions"
-  git config --global user.name "GitHub Actions"
   git add PKGBUILD .SRCINFO
   git commit -m "Update ${package}"
   git push origin master
